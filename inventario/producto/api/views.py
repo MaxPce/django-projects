b3a768f08fa5290377707e60aac18a7d279e8a3b
MaxPce <max.ponce@tecsup.edu.pe>
2025-09-14 20:36:37 -0500

    jwt-settings commit

--- inventario/producto/api/views.py
+++ inventario/producto/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from producto.models import Producto
 from producto.api.serializers import ProductoSerializer
 
 class ProductoViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer 
